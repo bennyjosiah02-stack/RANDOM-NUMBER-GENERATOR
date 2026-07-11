fb74232f6bd1644d3741186675c5a5c049a88d9a
Nureal Bazier <bennyjosiah02@gmail.com>
2026-07-11 19:03:42 +0530

    Fix GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/random-number-generator/',
+  base: '/RANDOM-NUMBER-GENERATOR/',
   plugins: [react()],
 })
